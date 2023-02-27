fab4644cd934e93fedae0fd440de9ae9d8a6815f
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2023-02-27 13:54:08 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -1,63 +1,49 @@
-import smtplib
 from socket import *
 
 def smtp_client(port=1025, mailserver='127.0.0.1'):
-    msg = "\r\n My message"
-    endmsg = "\r\n.\r\n"
-
+    # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((mailserver, port))
 
     recv = clientSocket.recv(1024).decode()
 
+    # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
 
-    sender = "MAIL FROM:<from@fromdomain.com>\r\n"
-    clientSocket.send(mailFromCommand.encode())
+    # Send MAIL FROM command and handle server response.
+    mailfrom = "MAIL FROM:<from@example.com>\r\n"
+    clientSocket.send(mailfrom.encode())
     recv2 = clientSocket.recv(1024).decode()
 
-    receivers = "RCPT TO:<my@emailadress.com'>\r\n"
-    clientSocket.send(rcptToCommand.encode())
+    # Send RCPT TO command and handle server response.
+    rcptto = "RCPT TO:<to@example.com>\r\n"
+    clientSocket.send(rcptto.encode())
     recv3 = clientSocket.recv(1024).decode()
 
-    dataCommand = "DATA\r\n"
-    clientSocket.send(dataCommand.encode())
+    # Send DATA command and handle server response.
+    data = "DATA\r\n"
+    clientSocket.send(data.encode())
     recv4 = clientSocket.recv(1024).decode()
 
-    clientSocket.send(msg.encode())
+    # Send message data.
+    message = "Subject: Test Message\r\n\r\nThis is a test email message.\r\n"
+    clientSocket.send(message.encode())
 
+    # Message ends with a single period, send message end and handle server response.
+    endmsg = "\r\n.\r\n"
     clientSocket.send(endmsg.encode())
     recv5 = clientSocket.recv(1024).decode()
 
-    quitCommand = "QUIT\r\n"
-    clientSocket.send(quitCommand.encode())
+    # Send QUIT command and handle server response.
+    quitmsg = "QUIT\r\n"
+    clientSocket.send(quitmsg.encode())
     recv6 = clientSocket.recv(1024).decode()
 
     clientSocket.close()
 
-    if name == 'main':
-        smtp_client(1025, '127.0.0.1')
 
+if __name__ == '__main__':
+    smtp_client(1025, '127.0.0.1')
 
-
-
-sender = 'from@fromdomain.com'
-receivers = ['my@emailadress.com']
-
-message = """From: From Person <from@fromdomain.com>
-To: To Person <to@todomain.com>
-Subject: SMTP e-mail test
-
-This is a test e-mail message.
-"""
-
-try:
-    smtpObj = smtplib.SMTP('127.0.0.1', 1025)
-    smtpObj.sendmail(sender, receivers, message)
-    print ("Successfully sent email")
-except smtplib.SMTPException:
-    print ("Error: unable to send email")
-
- 
