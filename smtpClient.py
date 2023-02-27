7cd6c9951fc647673567219a0d62e8d63a5f2eaf
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2023-02-27 12:22:30 -0500

    Add files via upload

---
+++ smtpClient.py
@@ -0,0 +1,18 @@
+import smtplib
+
+sender = 'from@fromdomain.com'
+receivers = ['my@emailadress.com']
+
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+
+try:
+    smtpObj = smtplib.SMTP('127.0.0.1', 1025)
+    smtpObj.sendmail(sender, receivers, message)
+    print ("Successfully sent email")
+except smtplib.SMTPException:
+    print ("Error: unable to send email")
