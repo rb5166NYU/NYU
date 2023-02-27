3bd49bc048f2e88d30ae0ab3379373e6301056f5
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2023-02-27 13:06:05 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -1,4 +1,47 @@
 import smtplib
+from socket import *
+
+def smtp_client(port=1025, mailserver='127.0.0.1'):
+    msg = "\r\n My message"
+    endmsg = "\r\n.\r\n"
+
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, port))
+
+    recv = clientSocket.recv(1024).decode()
+
+    heloCommand = 'HELO Alice\r\n'
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
+
+    sender = "MAIL FROM:<from@fromdomain.com>\r\n"
+    clientSocket.send(mailFromCommand.encode())
+    recv2 = clientSocket.recv(1024).decode()
+
+    receivers = "RCPT TO:<my@emailadress.com'>\r\n"
+    clientSocket.send(rcptToCommand.encode())
+    recv3 = clientSocket.recv(1024).decode()
+
+    dataCommand = "DATA\r\n"
+    clientSocket.send(dataCommand.encode())
+    recv4 = clientSocket.recv(1024).decode()
+
+    clientSocket.send(msg.encode())
+
+    clientSocket.send(endmsg.encode())
+    recv5 = clientSocket.recv(1024).decode()
+
+    quitCommand = "QUIT\r\n"
+    clientSocket.send(quitCommand.encode())
+    recv6 = clientSocket.recv(1024).decode()
+
+    clientSocket.close()
+
+    if name == 'main':
+        smtp_client(1025, '127.0.0.1')
+
+
+
 
 sender = 'from@fromdomain.com'
 receivers = ['my@emailadress.com']
@@ -17,5 +60,4 @@ try:
 except smtplib.SMTPException:
     print ("Error: unable to send email")
 
-    if name == 'main':
-        smtp_client(1025, '127.0.0.1')
+ 
