5e0f41ce7b0515a58b6a0565757831ada7ba2035
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-08 14:07:44 -0700

    Create Number Input.c

---
+++ Number Input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int usf, euf; /* Defines the usf and euf variables */
+     printf("Enter US Floor\n"); /* prints the statment */
+    scanf("%d", &usf); /* asks for a integer input and will define that to usf with "&" */
+    euf = usf - 1; /* defines that euf is usf minus 1 */
+    printf("EU Floor %d\n", euf); /* will pring the string "EU Floor and the number we did for the input. the %d is the placeholder for that number and we take that from the euf*/
+}
