cd320bbad3139027213d215d31301fdf889938e6
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-08 14:26:17 -0700

    Update String Input.c

--- String Input.c
+++ String Input.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    char name[100]; /* Declares a character array of 100 */
+    char name[100]; /* Declares a character array of 100... the "name" here is the variable being assigned.. The char is saying that assigned variable is an array (of characters) */
     printf("Enter Name\n"); /* Prints a prompt */
     scanf("%100s", name); /* asking for a string (%s) and only read up to 100 character (the 100 between % s). and assigns that input to "name" */
     printf("Hello %s\n", name); /* Prints Hello and then will look for a string and use "name" to fill in that string*/
