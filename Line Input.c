6188ec67207f7aae6dc8e8de13021f8a9dfb1698
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-08 16:12:15 -0700

    Update Line Input.c

--- Line Input.c
+++ Line Input.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main() {
-    char line[1001];/* */
-    printf("Enter line\n");/* */
-    scanf("%1000[^\n]s", line);/* */
+    char line[1001];/* an array defined as "line" that will take up to 1001 characters */
+    printf("Enter line\n");/* prompts user */
+    scanf("%1000[^\n]s", line);
+    /* [^\n] means match any character that's not a new line... 
+    scan up to the end of a line... 
+    Or until you hit 1000 character (defined with the 1000)*/
     printf("Line: %s\n", line); /* */
 }
