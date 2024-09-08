8b7dcb4b9b15e3fcfe30fc3db1d54e4c6b27a553
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-08 13:33:15 -0700

    Update printf.c

--- printf.c
+++ printf.c
@@ -7,6 +7,9 @@ int main() {
   printf("Hello world\n"); /* Hello World */
   printf("Answer %d\n", 42); /* Answer 42 */
   printf("Name %s\n", "Ryan"); /* Name Ryan */
+  /* the %.1f instructs the function to print a floating-point number with one digit after the decimal point.
+  More specifically for %.1f will dictate how many decimal spots you have. If I change it to 5 there will then be 5 decimal spots*/
+  /* %d indicates it is an integer..a number */
   printf("x %.1f i %d\n", 3.5, 100); /* x 3.5 i 100 */
 
 }
