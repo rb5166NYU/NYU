8c2064938815076dc9eee3e2cb8e662202b6230d
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-08 14:22:03 -0700

    Create String Input.c

---
+++ String Input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char name[100]; /* Declares a character array of 100 */
+    printf("Enter Name\n"); /* Prints a prompt */
+    scanf("%100s", name); /* asking for a string (%s) and only read up to 100 character (the 100 between % s). and assigns that input to "name" */
+    printf("Hello %s\n", name); /* Prints Hello and then will look for a string and use "name" to fill in that string*/
+}
+
+
+/* NOTE: No need to add a "&" for an array. Using a & defines where to start. An array is technically stating where to start */
