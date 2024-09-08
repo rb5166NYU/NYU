325bfc221a9c45a035e02dfdd7b7c8e4ce8dba9a
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-08 13:42:19 -0700

    Update printf.c

--- printf.c
+++ printf.c
@@ -5,6 +5,7 @@ int main() {
 /* comments next to the line will show the intented output */
   /* the \n is what says to print a new line, if removed they will all be in the same line */
   printf("Hello world\n"); /* Hello World */
+  /* the %d is telling us there is an integer (number) and to convert that into a string */
   printf("Answer %d\n", 42); /* Answer 42 */
   printf("Name %s\n", "Ryan"); /* Name Ryan */
   /* the %.1f instructs the function to print a floating-point number with one digit after the decimal point.
