b34541471afa33cfe238d68e35ff716f984fb455
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-08 13:27:37 -0700

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* This is a comment */
+
+int main() {
+/* comments next to the line will show the intented output */
+  
+  printf("Hello world\n"); /* Hello World */
+  printf("Answer %d\n", 42); /* Answer 42 */
+  printf("Name %s\n", "Ryan"); /* Name Ryan */
+  printf("x %.1f i %d\n", 3.5, 100); /* x 3.5 i 100 */
+
+}
+
