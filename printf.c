ff658efa351417237296a18728461d47bcede5ca
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-08 13:37:38 -0700

    Update printf.c

--- printf.c
+++ printf.c
@@ -10,6 +10,9 @@ int main() {
   /* the %.1f instructs the function to print a floating-point number with one digit after the decimal point.
   More specifically for %.1f will dictate how many decimal spots you have. If I change it to 5 there will then be 5 decimal spots*/
   /* %d indicates it is an integer..a number */
+  /* To tie the bottom together... the %.1f will take in the first number specified (the 3.5) while.
+  While the %d will take in the second number specified (the 100)
+  HOWEVER, the %.1f needs a number with a decimal place, while the %d needs a whole number*/
   printf("x %.1f i %d\n", 3.5, 100); /* x 3.5 i 100 */
 
 }
