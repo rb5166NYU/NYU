7228c4812374c8f5afb5ef3048ff822120cdbdad
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-14 11:22:27 -0700

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -3,13 +3,13 @@
 // int main is defining the main function
 // int number declares a variable named "number" 
 int main() {
-    int number;
+    int year;
 
 //printf will ask what year you'd like to enter
     printf("Enter a year: ");
 //scanf will read the number you enter and assign it to the number variable
 //The & provides the address to the number variable so it can be stored there
-    scanf("%d", &number);
+    scanf("%d", &year);
 
 /*
 This line first checks if the year is divisable by 4
@@ -18,16 +18,16 @@ The && (and) ensure it is divisable by 4 AND then will check to make sure it is 
 THEN it will check to make sure it can be divisable by 400 with the || (OR). 
 Meaning either divisable by 4 OR 400 but NOT 100.
 */
-    if ((number % 4 == 0) && (number % 100 != 0 || number % 400 == 0)) {
+    if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
 // The %d tells the printf to expect an integer value
 // The \n moves the cursor to the next line after printing the text
 // "number" is the variable that will be assigned to the %d
-        printf("%d was a leap year", number);
+        printf("%d was a leap year", year);
     } else {
-        printf("%d was not a leap year", number);
+        printf("%d was not a leap year", year);
     }
-       
-
+    
+    
     return 0;
 
 }
