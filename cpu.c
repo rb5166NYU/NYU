c60406f3625972d1941a4e4319f5f5569dd64ad7
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-09-20 13:00:09 -0700

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -116,40 +116,38 @@ struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *q
 PART 3
 
 *******************************************************/
-
 struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int time_stamp) {
-
-    if (test_null_pcb(current_process)) { // Checks if current process is null
-        new_process.execution_starttime = time_stamp; // setting the new process start time to the current time stamp
-        new_process.execution_endtime = time_stamp + new_process.total_bursttime; // setting the endtime to equal the current time plus the total run time 
-        new_process.remaining_bursttime = new_process.total_bursttime; // Setting the  new process remaining run time to the total run time 
-        return new_process; // indicates the new process is now the active process
+    if (test_null_pcb(current_process)) { // No currently-running process
+        new_process.execution_starttime = time_stamp; // Set start time
+        new_process.execution_endtime = time_stamp + new_process.total_bursttime; // Set end time
+        new_process.remaining_bursttime = new_process.total_bursttime; // Initialize remaining time
+        return new_process; // Return new process
     }
 
-    
-    else if (current_process.remaining_bursttime < new_process.remaining_bursttime) { // If the current process remaining run time is less than the new process remaining run time
-        new_process.execution_starttime = 0; // sets start time to 0
-        new_process.execution_endtime = 0;// sets endtime to 0
-        new_process.remaining_bursttime = new_process.total_bursttime; // sets remaing run time to total run time.. Meaning time is reset
-        ready_queue[*queue_cnt] = new_process; // sets the queue count pointer to the new process.. Adding it to the ready queue to the position inidcated by the pointer
-        *queue_cnt = *queue_cnt + 1; // adds one to the queue count. ensuring the next process is added to the correct index
-        return current_process;
-    }
+    // New process has a shorter remaining burst time
+    if (new_process.remaining_bursttime < current_process.remaining_bursttime) {
+        // Preempt current process
+        current_process.execution_endtime = 0; // Mark as not finished
+        current_process.remaining_bursttime -= (time_stamp - current_process.execution_starttime); // Update remaining time
+        ready_queue[*queue_cnt] = current_process; // Add current process to ready queue
+        (*queue_cnt)++; // Increment queue count
 
-    // If new process has the shortest remaining time
-    else {
+        // Initialize new process
         new_process.execution_starttime = time_stamp; 
         new_process.execution_endtime = time_stamp + new_process.total_bursttime;
         new_process.remaining_bursttime = new_process.total_bursttime;
-        current_process.execution_starttime = 0;
-        current_process.execution_endtime = 0;
-        ready_queue[*queue_cnt] = current_process; // adding current process to ready queue
-        *queue_cnt = *queue_cnt + 1;
-        return new_process;
+
+        return new_process; // Return new process
+    } else {
+        // If the new process does not preempt the current process
+        new_process.execution_starttime = 0; // Set start time to 0
+        new_process.execution_endtime = 0; // Set end time to 0
+        new_process.remaining_bursttime = new_process.total_bursttime; // Reset remaining time
+        ready_queue[*queue_cnt] = new_process; // Add new process to ready queue
+        (*queue_cnt)++; // Increment queue count
+        return current_process; // Return current process
     }
 }
-
-
 
 
 
