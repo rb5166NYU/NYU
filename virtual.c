e1042442035a0dc6b3e444cf5185ed8100be9b19
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-10-20 12:01:04 -0700

    Update virtual.c

--- virtual.c
+++ virtual.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct PTE {
-    int is_valid;
-    int frame_number;
-    int arrival_timestamp;
-    int last_access_timestamp;
-    int reference_count;
-};
-
 // Helper functions
 void update_page_entry(struct PTE *entry, int frame_number, int current_timestamp) {
     entry->is_valid = 1;
@@ -29,21 +21,18 @@ void evict_page(struct PTE *entry) {
 int find_page_to_evict(struct PTE page_table[TABLEMAX], int table_cnt, const char *strategy) {
     int eavi = -1;
     if (strategy == "FIFO") {
-        // Implement FIFO logic to find the earliest arriving valid page
         for (int i = 0; i < table_cnt; i++) {
             if (page_table[i].is_valid && (eavi == -1 || page_table[i].arrival_timestamp < page_table[eavi].arrival_timestamp)) {
                 eavi = i;
             }
         }
     } else if (strategy == "LRU") {
-        // Implement LRU logic
         for (int i = 0; i < table_cnt; i++) {
             if (page_table[i].is_valid && (eavi == -1 || page_table[i].last_access_timestamp < page_table[eavi].last_access_timestamp)) {
                 eavi = i;
             }
         }
     } else if (strategy == "LFU") {
-        // Implement LFU logic
         for (int i = 0; i < table_cnt; i++) {
             if (page_table[i].is_valid && (eavi == -1 || page_table[i].reference_count < page_table[eavi].reference_count || 
                 (page_table[i].reference_count == page_table[eavi].reference_count && page_table[i].arrival_timestamp < page_table[eavi].arrival_timestamp))) {
@@ -55,21 +44,18 @@ int find_page_to_evict(struct PTE page_table[TABLEMAX], int table_cnt, const cha
 }
 
 int process_page_access(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp, const char *strategy) {
-    // If the page is already in memory
     if (page_table[page_number].is_valid) {
         page_table[page_number].last_access_timestamp = current_timestamp;
         page_table[page_number].reference_count++;
         return page_table[page_number].frame_number;
     }
 
-    // If there's a free frame
     if (*frame_cnt > 0) {
         int next_frame = frame_pool[--(*frame_cnt)];
         update_page_entry(&page_table[page_number], next_frame, current_timestamp);
         return page_table[page_number].frame_number;
     }
 
-    // No free frames; need to evict a page
     int eavi = find_page_to_evict(page_table, *table_cnt, strategy);
     evict_page(&page_table[eavi]);
     update_page_entry(&page_table[page_number], page_table[eavi].frame_number, current_timestamp);
@@ -81,11 +67,11 @@ int count_page_faults_fifo(struct PTE page_table[TABLEMAX], int table_cnt, int r
     int timestamp = 0;
     for (int i = 0; i < reference_cnt; i++) {
         int j = reference_string[i]; 
-        if (page_table[j].is_valid) {
-            page_table[j].last_access_timestamp = timestamp; 
-        } else {
+        if (!page_table[j].is_valid) {
             faults++;
             process_page_access(page_table, &table_cnt, j, frame_pool, &frame_cnt, timestamp, "FIFO");
+        } else {
+            page_table[j].last_access_timestamp = timestamp; 
         }
         timestamp++;
     }
@@ -97,11 +83,11 @@ int count_page_faults_lru(struct PTE page_table[TABLEMAX], int table_cnt, int re
     int timestamp = 0;
     for (int i = 0; i < reference_cnt; i++) {
         int j = reference_string[i]; 
-        if (page_table[j].is_valid) {
-            page_table[j].last_access_timestamp = timestamp; 
-        } else {
+        if (!page_table[j].is_valid) {
             faults++;
             process_page_access(page_table, &table_cnt, j, frame_pool, &frame_cnt, timestamp, "LRU");
+        } else {
+            page_table[j].last_access_timestamp = timestamp; 
         }
         timestamp++;
     }
@@ -113,16 +99,15 @@ int count_page_faults_lfu(struct PTE page_table[TABLEMAX], int table_cnt, int re
     int timestamp = 0;
     for (int i = 0; i < reference_cnt; i++) {
         int j = reference_string[i]; 
-        if (page_table[j].is_valid) {
-            page_table[j].last_access_timestamp = timestamp; 
-        } else {
+        if (!page_table[j].is_valid) {
             faults++;
             process_page_access(page_table, &table_cnt, j, frame_pool, &frame_cnt, timestamp, "LFU");
+        } else {
+            page_table[j].last_access_timestamp = timestamp; 
         }
         timestamp++;
     }
     return faults;
 }
-
 
 
