4aeaa85b2c0393e0b7462c1480b1c4d9363180a7
rb5166NYU <124468752+rb5166NYU@users.noreply.github.com>
2024-10-20 12:15:11 -0700

    Update virtual.c

--- virtual.c
+++ virtual.c
@@ -17,29 +17,29 @@ int process_page_access_fifo(struct PTE page_table[TABLEMAX], int *table_cnt, in
         page_table[page_number].arrival_timestamp = current_timestamp;
         page_table[page_number].last_access_timestamp = current_timestamp;
         page_table[page_number].reference_count = 1;
-        (*table_cnt)++;  // Update the table count
+        (*table_cnt)++;  // Update the table count for a new entry
         return new_frame;
     }
 
     // Replace a page using FIFO
     int oldest_index = -1;
-    for (int i = 0; i < TABLEMAX; i++) { // Use TABLEMAX instead of *table_cnt
+    for (int i = 0; i < TABLEMAX; i++) { // Loop through all entries
         if (page_table[i].is_valid && (oldest_index == -1 || page_table[i].arrival_timestamp < page_table[oldest_index].arrival_timestamp)) {
             oldest_index = i;
         }
     }
 
     // Replace the oldest page
-    page_table[oldest_index].is_valid = 0;
+    page_table[oldest_index].is_valid = 0; // Mark it as invalid
 
     // Insert the new page
-    int new_frame = page_table[oldest_index].frame_number;
-    page_table[page_number].frame_number = new_frame;
+    page_table[oldest_index].frame_number = page_table[page_number].frame_number; // Reuse frame number
+    page_table[page_number].frame_number = page_table[oldest_index].frame_number; // Set frame for new page
     page_table[page_number].is_valid = 1;
     page_table[page_number].arrival_timestamp = current_timestamp;
     page_table[page_number].last_access_timestamp = current_timestamp;
     page_table[page_number].reference_count = 1;
-    return new_frame;
+    return page_table[page_number].frame_number; // Return new frame
 }
 
 int count_page_faults_fifo(struct PTE page_table[TABLEMAX], int table_cnt, int reference_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) {
@@ -81,23 +81,22 @@ int process_page_access_lru(struct PTE page_table[TABLEMAX], int *table_cnt, int
 
     // Replace a page using LRU
     int lru_index = -1;
-    for (int i = 0; i < TABLEMAX; i++) { // Use TABLEMAX instead of *table_cnt
+    for (int i = 0; i < TABLEMAX; i++) { // Loop through all entries
         if (page_table[i].is_valid && (lru_index == -1 || page_table[i].last_access_timestamp < page_table[lru_index].last_access_timestamp)) {
             lru_index = i;
         }
     }
 
     // Replace the least recently used page
-    page_table[lru_index].is_valid = 0;
+    page_table[lru_index].is_valid = 0; // Mark it as invalid
 
     // Insert the new page
-    int new_frame = page_table[lru_index].frame_number;
-    page_table[page_number].frame_number = new_frame;
+    page_table[page_number].frame_number = page_table[lru_index].frame_number; // Reuse frame number
     page_table[page_number].is_valid = 1;
     page_table[page_number].arrival_timestamp = current_timestamp;
     page_table[page_number].last_access_timestamp = current_timestamp;
     page_table[page_number].reference_count = 1;
-    return new_frame;
+    return page_table[page_number].frame_number; // Return new frame
 }
 
 int count_page_faults_lru(struct PTE page_table[TABLEMAX], int table_cnt, int reference_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) {
@@ -139,7 +138,7 @@ int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int
 
     // Replace a page using LFU
     int lfu_index = -1;
-    for (int i = 0; i < TABLEMAX; i++) { // Use TABLEMAX instead of *table_cnt
+    for (int i = 0; i < TABLEMAX; i++) { // Loop through all entries
         if (page_table[i].is_valid && (lfu_index == -1 || page_table[i].reference_count < page_table[lfu_index].reference_count || 
             (page_table[i].reference_count == page_table[lfu_index].reference_count && page_table[i].arrival_timestamp < page_table[lfu_index].arrival_timestamp))) {
             lfu_index = i;
@@ -147,16 +146,15 @@ int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int
     }
 
     // Replace the least frequently used page
-    page_table[lfu_index].is_valid = 0;
+    page_table[lfu_index].is_valid = 0; // Mark it as invalid
 
     // Insert the new page
-    int new_frame = page_table[lfu_index].frame_number;
-    page_table[page_number].frame_number = new_frame;
+    page_table[page_number].frame_number = page_table[lfu_index].frame_number; // Reuse frame number
     page_table[page_number].is_valid = 1;
     page_table[page_number].arrival_timestamp = current_timestamp;
     page_table[page_number].last_access_timestamp = current_timestamp;
     page_table[page_number].reference_count = 1;
-    return new_frame;
+    return page_table[page_number].frame_number; // Return new frame
 }
 
 int count_page_faults_lfu(struct PTE page_table[TABLEMAX], int table_cnt, int reference_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) {
